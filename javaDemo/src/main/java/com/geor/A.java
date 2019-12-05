32f60a85c4d3551e0114e9fee443b83f3adc9937
yafeisong <longneilongwai@163.com>
2019-12-05 16:15:41 +0800

    Update A.java

--- javaDemo/src/main/java/com/geor/A.java
+++ javaDemo/src/main/java/com/geor/A.java
@@ -15,6 +15,7 @@ public class A {
 		Set<String> areas = new HashSet<String>();
 		areas.add("江苏");
 		areas.add("江苏");
+		areas.add("江苏");
 		areas.add("南京");
 		areas.add("苏州");
 		areas.add("无锡");
